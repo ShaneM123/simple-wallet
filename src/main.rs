161cbf5e93295a10e32e10ddbc636d809c6f465e
ShaneM123 <shanemoloneybusiness@gmail.com>
2021-07-31 16:55:03 +0200

    added button events

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
-  use web3;
+  use secp256k1::{PublicKey, SecretKey};
+use web3;
   use tokio;
   use web3::types::{Address};
-  use std::str::FromStr;
+  use core::panic;
+use std::{collections::HashMap, str::FromStr};
   use crate::wallet_lib::{create_txn_object, sign_and_send};
   use anyhow::{Result};
   use fltk::{app, button::Button, frame::Frame, prelude::*, window::Window};
@@ -16,7 +18,10 @@
   // make it into a library with functions I can call
   // call those functions from the interface on button clicks
   // make an interface
-
+#[derive(Clone,Debug)]
+  pub enum WalletMessage{
+    NewWallet,
+  }
 
   #[tokio::main]
    async fn main() -> Result<()> {
@@ -27,18 +32,38 @@
       .with_size(600, 100)
       .center_of(&wind)
       .with_label("0 Wallets");
-      
+      let web3 = wallet_lib::establish_web3_connection(URL)?;
+      let mut accounts = web3.eth().accounts().await?;
+      let mut keypairs: Vec<(PublicKey, SecretKey)> = Vec::new();
       wind.end();
       wind.show();
 
-      but.set_callback(move |_| frame.set_label("New Wallet Created!"));
+    let (s, r) = app::channel::<WalletMessage>();
 
+    but.emit(s, WalletMessage::NewWallet);
+    // but_dec.emit(s, Message::Decrement);
+    
+    while app.wait() {
+        if let Some(msg) = r.recv() {
+            match msg {
+                WalletMessage::NewWallet => {
+                        let (seckey, pubkey) =
+      match  wallet_lib::create_keypair() {
+        Ok(val) => val,
+        //todo: implement anyhow error handling here
+        Err(e) => unimplemented!(),
+      };
+                  keypairs.push((pubkey, seckey));
+                  frame.set_label(&format!("{} Wallets", keypairs.len()));
+                  println!("keypairs {:?}", keypairs);
+                },
+            }
+        }
+    }
      app.run().unwrap();
-
     // let (seckey, pubkey) = wallet_lib::create_keypair()?;
     // println!("public key: {}", pubkey);
     //
-    // let web3 = wallet_lib::establish_web3_connection(URL)?;
     //
     // let to = Address::from_str("0x08302CF8648A961c607e3e7Bd7B7Ec3230c2A6c5").unwrap();
     // let tx_object = create_txn_object(to, 7)?;
